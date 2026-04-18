6e26ddd5aa8a4b35de6433e7f6c18c173e2cdb39
csy870617 <csy0645009@gmail.com>
2026-04-18 12:21:43 -0700

    Add seek support to TTS progress bar

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import { renderEntries, renderTabs, renderFolders, closeAllModals, openModal, op
 import { openEditor, toggleViewMode, formatDoc, changeGlobalFontSize, changeGlobalFontFamily, insertSticker, applyFontStyle, turnPage, jumpToPage, insertImage, triggerAutoSave, insertTable, createHyperlink, addRow, deleteRow, addColumn, deleteColumn, openTableInsertModal, openTableEditModal, mergeCells, saveCurrentSelection, increaseFontSize, decreaseFontSize, detectSelectionFontSize } from './editor.js';
 import { setupAuthListeners } from './auth.js';
 import { initGoogleDrive, handleAuthClick, saveToDrive, syncFromDrive, ensureTokenOnResume, startKeepAlive } from './drive.js';
-import { toggleTTSPanel, toggleTTSSettings, playTTS, pauseTTS, stopTTS, setTTSStart, setTTSEnd, resetTTSRange, playSelection, updateSpeedDisplay, updatePitchDisplay, updateGapDisplay, initTTS, updateTTSRange } from './tts.js';
+import { toggleTTSPanel, toggleTTSSettings, playTTS, pauseTTS, stopTTS, setTTSStart, setTTSEnd, resetTTSRange, playSelection, updateSpeedDisplay, updatePitchDisplay, updateGapDisplay, initTTS, updateTTSRange, seekTTSByPercent } from './tts.js';
 
 window.addNewCategory = addNewCategory;
 window.restoreEntry = restoreEntry;
@@ -452,6 +452,7 @@ function setupUIListeners() {
     document.getElementById('tts-speed-slider')?.addEventListener('input', updateSpeedDisplay);
     document.getElementById('tts-pitch-slider')?.addEventListener('input', updatePitchDisplay);
     document.getElementById('tts-gap-slider')?.addEventListener('input', updateGapDisplay);
+    document.getElementById('tts-progress-slider')?.addEventListener('input', (e) => seekTTSByPercent(e.target.value));
     initTTS();
 
     document.getElementById('toolbar-link-btn')?.addEventListener('click', () => { createHyperlink(); });
